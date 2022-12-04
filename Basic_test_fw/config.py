11e56197223d7dcc8c4864a72dd7ee5de432aa66
Arun Prakasam Soundappan <asoundappan@synamedia.com>
2022-12-04 09:01:15 +0530

    Single SESSION iso multiple auth calls

---
+++ Basic_test_fw/config.py
@@ -0,0 +1,8 @@
+import requests
+
+SESSION = requests.session()
+
+APP_URL = "http://10.78.213.212:8080"
+
+ADMIN_USER = 'admin'
+ADMIN_PASSWORD = 'admin'
--- Basic_test_fw/lib/comments.py
+++ Basic_test_fw/lib/comments.py
@@ -1,12 +1,13 @@
-import requests
 from lib.utils import build_request_headers
+from config import SESSION
 
 class Comments:
     
     def __init__(self):
+        # Define and select resource based on the given version if needed
         self.comment_url = "/comments"
 
     def get_all_comments(self, app_url, access_token):
         request_headers = build_request_headers(access_token)
-        response = requests.get(f"{app_url}{self.comment_url}", headers=request_headers)
+        response = SESSION.get(f"{app_url}{self.comment_url}", headers=request_headers)
         return response
